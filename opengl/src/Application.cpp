1d183707da6ee47ba37c241cae5fe67307c22353
Kartik K <kartik.karia09@gmail.com>
2020-05-26 18:40:33 +0530

    added basic texture

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -14,8 +14,8 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "Logger.h"
+#include "Texture.h"
 
-//hello there
 
 int main(void)
 {
@@ -29,7 +29,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -46,10 +46,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
   {
 	float positions[] = {
-		-0.5f, -0.5f,
-		 0.5f, -0.5f,
-		 0.5f,  0.5f,
-		-0.5f,  0.5f
+		-0.75f,-0.75f, 0.0f, 0.0f,
+		 0.75f, -0.75f, 1.0f, 0.0f,
+		 0.75f,  0.75f, 1.0f, 1.0f,
+		-0.75f,  0.75f, 0.0f, 1.0f
 	};
 
 	unsigned int indices[] = {
@@ -57,10 +57,14 @@ int main(void)
 		2,3,0
 	};
 
+	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+	GLCall(glEnable(GL_BLEND));
+
 	VertexArray va;
-	VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+	VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 	VertexBufferLayout layout;
+	layout.Push<float>(2);
 	layout.Push<float>(2);
 	va.AddBuffer(vb,layout);
 
@@ -69,8 +73,11 @@ int main(void)
 
 	Shader shader("res/shaders/Basic.shader");
 	shader.Bind();
-	shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+	//shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 	
+	Texture texture("res/textures/kk.png");
+	texture.Bind(0);
+	shader.SetUniform1i("u_Texture", 0);
 
 	va.Unbind();
 	vb.Unbind();
--- opengl/src/Shader.h
+++ opengl/src/Shader.h
@@ -29,6 +29,7 @@ public:
 
 	
 	//Set Uniform
+	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
@@ -40,6 +41,6 @@ private:
 
 
 
-	unsigned int GetUniformLocation(const std::string&  name);
+	int GetUniformLocation(const std::string&  name);
 
 }; 
